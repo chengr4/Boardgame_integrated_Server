7f4b01d1cb61fa7fcec1d99a8f75c3de68deab85
Eddie Cheng <54136390+fscheng4747@users.noreply.github.com>
2020-09-28 12:43:57 +0200

    files to deploy app on Heroku

--- application.py
+++ application.py
@@ -5,9 +5,6 @@ from app import application
 
 # run the app.
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = True
     application.run()
 
 
