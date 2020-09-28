4e48962a6641135fe688f4f10268446d528835d7
Eddie Cheng <54136390+fscheng4747@users.noreply.github.com>
2020-09-28 14:47:15 +0200

    added database

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,13 @@ from flask_login import LoginManager
 application = Flask(__name__)
 #  for cross-site request forgery agianst attack
 application.config['SECRET_KEY'] = '4e57a23642728784e88ef62c418e1318'
-# set database uri
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+# set database uri (local)
+#application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+# set database uri (online)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://drvdyfommfmtgt:'+
+'a18dd8832f85b2002fd471d3f032875345fff0eb45e69cfbae88cc47e99f2c60@ec2-3-224-97-209.compute-1.amazonaws.com:5432/dd91muj73h8bjd'
 
 # to initialize
 db = SQLAlchemy(application)
