1b43cf44ad6611bb3f094ac2ca73f379978fcf90
Eddie Cheng <54136390+fscheng4747@users.noreply.github.com>
2020-10-02 16:59:19 +0200

    string of title limit 100 -> 10

--- app/models.py
+++ app/models.py
@@ -34,7 +34,7 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     '''Interface for post table'''
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(10), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     # foreign key
